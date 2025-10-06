f90929768c79475f6cfeec79bd9b3826f4a1b57e
Polina Lepekhina <P0lliSun@yandex.ru>
2025-10-06 15:30:18 +0300

    fix component Partnership

--- src/pages/Partnership/Partnership.tsx
+++ src/pages/Partnership/Partnership.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Popup from '../../components/Popups/Popup/Popup';
 import Form from '../../components/Form/Form';
 import { useTranslation } from 'react-i18next';
@@ -13,11 +13,13 @@ import Textarea from '../../components/Textarea/Textarea';
 import { usePartnership } from '../../utils/hooks/usePartnership/usePartnership';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import Preloader from '../../components/Preloader/Preloader';
+import PopupPartnershipThanks from './PopupPartnershipThanks/PopupPartnershipThanks';
 
 const Partnership: FC = () => {
     const { t } = useTranslation();
     const navigate = useNavigate();
     const { mutateAsync, isPending, error } = usePartnership();
+    const [showPopup, setShopPopup] = useState(false);
 
     const onClose = () => {
         navigate('/');
@@ -38,25 +40,28 @@ const Partnership: FC = () => {
             user_name: data.nameContact,
             restaurant_name: data.nameCompany,
         });
-        navigate('/');
+        setShopPopup(true);
     };
 
     return (
-        <Popup title={t('pages.partnership.title')} onClose={onClose}>
-            {isPending && <Preloader />}
-            <Form name="form-partnership" onSubmit={handleSubmit(onSubmit)}>
-                <FormInputs>
-                    <Input type="text" name="nameCompany" placeholder={t('pages.partnership.placeholderNameCompany')} nameLabel={t('pages.partnership.nameLabelNameCompany')} register={register} errors={errors} pattern={regexClientName}></Input>
-                    <Input type="text" name="address" placeholder={t('pages.partnership.placeholderAddress')} nameLabel={t('pages.partnership.nameLabelAddress')} register={register} errors={errors} pattern={regexAddress}></Input>
-                    <Input type="text" name="nameContact" placeholder={t('pages.partnership.placeholderNameContact')} nameLabel={t('pages.partnership.nameLabelNameContact')} register={register} errors={errors} pattern={regexClientName}></Input>
-                    <Input type="text" name="email" placeholder={t('pages.partnership.placeholderEmail')} nameLabel={t('pages.partnership.nameLabelEmail')} register={register} errors={errors} pattern={regexEmail}></Input>
-                    <InputPhone register={register} errors={errors}></InputPhone>
-                    <Textarea name="message" nameLabel={t('pages.partnership.nameLabelMessage')} placeholder={t('pages.partnership.placeholderMessage')} register={register} errors={errors} pattern={/^[\s\S]{10,5000}$/}></Textarea>
-                </FormInputs>
-                <Button type="submit">{t('pages.partnership.buttonSendRequest')}</Button>
-            </Form>
-            {error && <ErrorMessage message={error.message} />}
-        </Popup>
+        <>
+            <Popup title={t('pages.partnership.title')} onClose={onClose}>
+                {isPending && <Preloader />}
+                <Form name="form-partnership" onSubmit={handleSubmit(onSubmit)}>
+                    <FormInputs>
+                        <Input type="text" name="nameCompany" placeholder={t('pages.partnership.placeholderNameCompany')} nameLabel={t('pages.partnership.nameLabelNameCompany')} register={register} errors={errors} pattern={regexClientName}></Input>
+                        <Input type="text" name="address" placeholder={t('pages.partnership.placeholderAddress')} nameLabel={t('pages.partnership.nameLabelAddress')} register={register} errors={errors} pattern={regexAddress}></Input>
+                        <Input type="text" name="nameContact" placeholder={t('pages.partnership.placeholderNameContact')} nameLabel={t('pages.partnership.nameLabelNameContact')} register={register} errors={errors} pattern={regexClientName}></Input>
+                        <Input type="text" name="email" placeholder={t('pages.partnership.placeholderEmail')} nameLabel={t('pages.partnership.nameLabelEmail')} register={register} errors={errors} pattern={regexEmail}></Input>
+                        <InputPhone register={register} errors={errors}></InputPhone>
+                        <Textarea name="message" nameLabel={t('pages.partnership.nameLabelMessage')} placeholder={t('pages.partnership.placeholderMessage')} register={register} errors={errors} pattern={/^[\s\S]{10,5000}$/}></Textarea>
+                    </FormInputs>
+                    <Button type="submit">{t('pages.partnership.buttonSendRequest')}</Button>
+                </Form>
+                {error && <ErrorMessage message={error.message} />}
+            </Popup>
+            {showPopup && <PopupPartnershipThanks />}
+        </>
     );
 };
 
