5821798ce48650babcc1bb9524f307afa96c19c3
SergeyGetmanskiy <getmansky1997@yandex.ru>
2025-10-12 18:49:07 +0300

    fix restaurants markers flickering on map

--- src/utils/hooks/useRestaurants/useRestaurants.ts
+++ src/utils/hooks/useRestaurants/useRestaurants.ts
@@ -1,20 +1,25 @@
 import { useQuery, skipToken } from '@tanstack/react-query';
-import { restaurantsService, SearchSuggestion } from '../../api/restaurantsService/restaurantsService';
+import { Restaurant, restaurantsService, SearchSuggestion } from '../../api/restaurantsService/restaurantsService';
 import { LngLat, LngLatBounds } from '@yandex/ymaps3-types';
 import { uniq } from 'lodash';
 import { VenueType } from '../../../contexts/RestaurantsContext';
+import { useRef } from 'react';
 
 export const useRestaurants = (bounds: LngLatBounds, userLocation: LngLat, selectedOptions: SearchSuggestion[], selectedVenueTypes: VenueType[]) => {
+    const cachedFilteredRestaurants = useRef<Restaurant[] | []>([]);
     const ids = uniq(selectedOptions.map((item) => item.restaurant_ids).flat());
     const types = selectedVenueTypes.map((item) => item.name);
-    const { isLoading, isError, isSuccess, data, refetch } = useQuery({
+    const { isLoading, isError, data, refetch } = useQuery({
         queryKey: ['restaurants', bounds, userLocation, ids, types],
         queryFn: bounds.length > 0 ? () => restaurantsService.getRestaurants(bounds as LngLatBounds, userLocation as LngLat, ids, types) : skipToken,
     });
+    if (data) {
+        cachedFilteredRestaurants.current = data.data;
+    }
     return {
         isLoading,
         isError,
-        restaurantsOnMap: isSuccess ? data.data : [],
+        restaurantsOnMap: cachedFilteredRestaurants.current,
         refetch,
     };
 };
