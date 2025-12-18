22f7415bc7ca873894d5d4d7723ab2a68e10430e
SergeyGetmanskiy <getmansky1997@yandex.ru>
2025-12-18 10:58:56 +0300

    get API URL from ENV

--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -13,7 +13,6 @@ export const regexEmail: RegExp = /.+@.+\..+/;
 export const regexAddress: RegExp = /^[a-zA-Zа-яА-ЯёЁӘәҒғҚқҢңӨөҰұҮүІі0-9\s.,/\-–—()"']{1,256}$/;
 export const regexMessage: RegExp = /^[\s\S]{10,5000}$/;
 
-export const API_URL = 'https://80.90.183.184';
 export const mealTypes: MealType[] = ['food', 'drink', 'dessert'];
 export const types = ['fastFood', 'cafe', 'cafeBar'].map((type) => {
     return { id: increment(), name: type, selected: false };
