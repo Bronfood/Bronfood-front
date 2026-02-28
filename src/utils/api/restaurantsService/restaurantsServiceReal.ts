7fcc7a2115a4b8bdef58164d7dc9e93c3bb471c5
SergeyGetmanskiy <getmansky1997@yandex.ru>
2026-02-28 16:22:37 +0300

    fix working time for selected restaurant

--- src/utils/api/restaurantsService/restaurantsServiceReal.ts
+++ src/utils/api/restaurantsService/restaurantsServiceReal.ts
@@ -50,7 +50,9 @@ export class RestaurantsServiceReal implements RestaurantsService {
         return { data: restaurants };
     }
     async getRestaurantById(id: number): Promise<{ data: Restaurant }> {
-        return handleFetch(`api/restaurants/${id}/`);
+        const responseData = await handleFetch(`api/restaurants/${id}/`);
+        const restaurant = this.addWorkingTime([responseData.data]);
+        return { data: restaurant[0] };
     }
     async getMeals(restaurantId: number): Promise<{ data: Meal[] }> {
         return handleFetch(`api/restaurants/${restaurantId}/meals/`);
