c1900efeca11c8e1a497871b7175fa1f396f8203
SergeyGetmanskiy <getmansky1997@yandex.ru>
2026-03-05 12:51:30 +0300

    add styles

--- src/pages/Restaurants/Restaurant/RestaurantInfoPopup/RestaurantInfoPopup.tsx
+++ src/pages/Restaurants/Restaurant/RestaurantInfoPopup/RestaurantInfoPopup.tsx
@@ -21,12 +21,21 @@ const RestaurantInfoPopup = ({ close, name, address, bin, directorFullname }: Re
         <div className={styles['info-popup']} onClick={handleOverlayClick}>
             <div className={styles['popup']}>
                 <h2 className={styles['popup__title']}>Юридическая информация</h2>
-                <p>{name}</p>
-                <p>{address}</p>
-                <p>{bin}</p>
-                <p>{directorFullname}</p>
+                <Paragraph label="Юридическое лицо" text={name} />
+                <Paragraph label="Юридический адрес" text={address} />
+                <Paragraph label="БИН" text={bin} />
+                <Paragraph label="Руководитель" text={directorFullname} />
                 <button className={styles['popup__close']} onClick={() => close()}></button>
             </div>
+        </div>
+    );
+};
+
+const Paragraph = ({ label, text }: { label: string; text: string }) => {
+    return (
+        <div className={styles['paragraph']}>
+            <span className={styles['paragraph__label']}>{label}</span>
+            <p className={styles['paragraph__text']}>{text}</p>
         </div>
     );
 };
