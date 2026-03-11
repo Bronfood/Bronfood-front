57a0e7e80f092339bfa07822755a1ec93262b200
SergeyGetmanskiy <getmansky1997@yandex.ru>
2026-03-11 20:32:59 +0300

    create title

--- src/pages/Admin/WorkStatus/WorkStatus.tsx
+++ src/pages/Admin/WorkStatus/WorkStatus.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+import styles from './WorkStatus.module.scss';
 import { useNavigate } from 'react-router-dom';
 import AdminPopup from '../AdminPopup/AdminPopup';
 import { DatePicker } from '../../../components/DatePicker/DatePicker';
+import { useTranslation } from 'react-i18next';
 
 function WorkStatus() {
     const [selectedDates, setSelectedDates] = useState<Date[] | undefined>();
+    const { t } = useTranslation();
     const navigate = useNavigate();
     const close = () => {
         navigate('/admin');
@@ -13,6 +16,7 @@ function WorkStatus() {
     return (
         <>
             <AdminPopup close={close}>
+                <h1 className={styles.title}>{t(`pages.admin.workStatus`)}</h1>
                 <DatePicker selected={selectedDates} setSelected={setSelectedDates} />
             </AdminPopup>
         </>
