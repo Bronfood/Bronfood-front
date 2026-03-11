912ad1a34e2bae930a203f9a6b374b03bdb4e606
SergeyGetmanskiy <getmansky1997@yandex.ru>
2026-03-11 13:36:36 +0300

    override default styles

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/style.css';
+import styles from './DatePicker.module.scss';
 
 export function DatePicker() {
     const [selected, setSelected] = useState<Date>();
 
-    return <DayPicker animate mode="single" selected={selected} onSelect={setSelected} footer={selected ? `Selected: ${selected.toLocaleDateString()}` : 'Pick a day.'} />;
+    return <DayPicker className={styles['rdp-root']} animate mode="single" selected={selected} onSelect={setSelected} footer={selected ? `Selected: ${selected.toLocaleDateString()}` : 'Pick a day.'} />;
 }
