12b5d698cb00d6c7bdf5a77f0698c1dfec7688d6
SergeyGetmanskiy <getmansky1997@yandex.ru>
2026-03-11 20:01:39 +0300

    lift state up

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/style.css';
 import './DatePicker.scss';
-
 import { ru } from 'react-day-picker/locale';
 
-export function DatePicker() {
-    const [selected, setSelected] = useState<Date[] | undefined>();
+type DatePickerProps = {
+    selected: Date[] | undefined;
+    setSelected: Dispatch<SetStateAction<Date[] | undefined>>;
+};
 
+export function DatePicker({ selected, setSelected }: DatePickerProps) {
     return <DayPicker animate mode="multiple" selected={selected} onSelect={setSelected} navLayout="around" locale={ru} showOutsideDays hideWeekdays />;
 }
--- src/pages/Admin/WorkStatus/WorkStatus.tsx
+++ src/pages/Admin/WorkStatus/WorkStatus.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AdminPopup from '../AdminPopup/AdminPopup';
 import { DatePicker } from '../../../components/DatePicker/DatePicker';
 
 function WorkStatus() {
+    const [selectedDates, setSelectedDates] = useState<Date[] | undefined>();
     const navigate = useNavigate();
     const close = () => {
         navigate('/admin');
@@ -11,7 +13,7 @@ function WorkStatus() {
     return (
         <>
             <AdminPopup close={close}>
-                <DatePicker />
+                <DatePicker selected={selectedDates} setSelected={setSelectedDates} />
             </AdminPopup>
         </>
     );
